0fa20cbcc131afb4de7bfb05dd75d82b77bfea36
NonymousMorlock <agamspaul@gmail.com>
2026-04-04 19:42:53 +0000

    🚀 initial commit

---
+++ android/app/src/main/kotlin/co/akundadababalei/physics_bubble/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.akundadababalei.physics_bubble
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:physics_bubble/app/app.dart';
+import 'package:physics_bubble/bootstrap.dart';
+
+Future<void> main() async {
+  await bootstrap(() => const App());
+}
